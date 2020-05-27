7235c90c0d645c3ffff457263be60209ee75c28e
NORNAZIRAH BINTE MOHAMED TALIB RP <17032457@myrp.edu.sg>
2020-05-27 13:17:07 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id17032457/demodatapassingtest/ThirdActivity.java
@@ -0,0 +1,25 @@
+package sg.edu.rp.c346.id17032457.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ThirdActivity extends AppCompatActivity {
+    TextView tvAnswer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        tvAnswer=findViewById(R.id.textView);
+        Intent intentReceived = getIntent();
+        char value = intentReceived.getCharExtra("value", 'X');
+        tvAnswer.setText("Character value received is: " + value);
+
+
+
+
+    }
+}
